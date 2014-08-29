76bd8c85e1496210528b1224f61069ac1248dc87
Siedrix <Siedrix@gmail.com>
2014-08-29 17:52:41 -0500

    First

---
+++ README.md
@@ -0,0 +1,12 @@
+This is the repo for nodebots.mx.
+
+
+To install dependencies do
+```
+	npm install
+```
+
+To run on local do
+```
+	supervisor -e js,html,json,md server.js
+```
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+	Paperpress = require('paperpress').Paperpress,
+	logger = require('morgan');
+
+var server = express();
+
+server.use(logger(':status :req[x-real-ip] :method :response-time ms :url'));
+server.use(express.static(__dirname + '/public'));
+
+var blog = new Paperpress({
+	directory : 'static',
+	themePath : 'static/layouts',
+	basePath  : '/blog',
+	articlesPerPage : 20,
+	pagesPath : ''
+});
+
+blog.attach(server);
+
+server.get('/', function (req, res) {
+	res.sendfile('./views/home.html');
+});
+
+server.get('/feed', function (req, res) {
+	res.redirect('/rss');
+});
+
+var webhook = require('./webhook');
+
+server.get('/webhook',webhook(blog));
+server.post('/webhook',webhook(blog));
+
+server.listen(4000);
+console.log('Server running at http://localhost:4000', new Date() );
